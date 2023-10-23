c52e0141e7a5371b9e20551320e7b8e517170bfe
ans3094 <won3094@gmail.com>
2023-10-23 21:52:43 +0900

    [add] 23일차

--- src/components/CreateItem.js
+++ src/components/CreateItem.js
@@ -1,3 +1,50 @@
+import { useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+
 export default function CreateItem(){
-    return <div></div>
+    const [values, setValues]= useState({
+        task:"",due:""
+    })
+
+    const hadleChange =(event)=>{
+        const {name,value} = event.target;
+        setValues({
+            ...values,
+            [name]: value,
+        })
+    }
+
+    const navigate= useNavigate();
+    // const ref = useRef();
+
+    // useEffect(()=>{
+    //     ref.current.focus();
+    // })
+
+    const onSubmit = (event) =>{
+        event.preventDefault();
+        fetch("http://localhost:3001/items",{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({task: values.task, due:values.due, status :"todo"})
+        }).then((res)=>{
+            if(res.ok){
+                alert('Created New Item !');
+                navigate("/todo")
+
+            }
+        })
+    }
+    return <form onSubmit={onSubmit}>
+        <div className="input_area">
+            <p>Task</p>
+            <input type="text" name="task" value={values.task} onChange={hadleChange} ></input>
+            <p>Due</p>
+            <input type="text" name="due" value={values.due} onChange={hadleChange}></input>
+        </div>
+        <button>Create</button>
+    </form>
 }
